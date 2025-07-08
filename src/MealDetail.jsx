2aec7fcde070a37f9e12d28125424b245fa1c8d3
Katelyn Mehner <kmehner@gmail.com>
2025-07-08 09:26:03 -0600

    finished meal detail component

--- src/MealDetail.jsx
+++ src/MealDetail.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import "./MealDetail.css"
 
 const MealDetail = ({ meal }) => {
 
+  if (!meal) return null; 
+
   return (
-    <div>
-      <p>MealDetail {meal.idMeal}</p>
-      <p>hello</p>
-      <h1>hi</h1>
+    <div className='meal-detail-card'>
+      <h2>{meal.strMeal}</h2>
+      <p className='meal-category'>Category: {meal.strCategory}</p>
+      <img src={meal.strMealThumb} alt={meal.strMeal} className='meal-image' />
+
+      <h3>Instructions:</h3>
+      <p className='meal-instructions'>{meal.strInstructions}</p>
+
     </div>
   )
 }
